6c7191165149788627aeae466c08e48383a3fe9b
Verchenko Ilya <via_atm@mail.ru>
2025-01-14 18:37:11 +0300

    test upload

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-class Task(models.Model):
 
-    status = models.BooleanField(default=False)
-    label = models.CharField(max_length=100)
-    description = models.TextField()
+class Task(models.Model):
+    _status_choices = (
+        ("TD","to do"),
+        ("IP", "in progress"),
+        ("DN","done"),
+    )
+
+    status = models.BooleanField(default=False, choices=_status_choices, verbose_name="Status")
+    label = models.CharField(max_length=100 , verbose_name="Label")
+    description = models.TextField(verbose_name="Description")
